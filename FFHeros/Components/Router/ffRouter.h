e207e8928f63db615040ed109831fe91d08160f0
ZhuDelun <zhudelun@bilibili.com>
2018-05-21 06:09:41 +0800

    update

--- FFHeros/Components/Router/ffRouter.h
+++ FFHeros/Components/Router/ffRouter.h
@@ -9,11 +9,30 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+DEPRECATED_MSG_ATTRIBUTE("并没有完全时限完成，暂时无法适配short url")
 @interface ffRouter : NSObject
 
 + (nonnull ffRouter *)shared;
 
+
+/**
+ 用短链接注册目标对象，短连接格式 "/abc/def/:param1/ghi/:param2 ..."
+
+ @code
+ [[ffRouter shared] map:@"/hero/:cid/comics"
+                toClass:[ffHeroDetailViewController class]];
+ @endcode
+ @param router 短连接 例如：/abc/def/:param1/ghi/:param2 ...
+ @param vcClass 需要被注册是类型
+ */
 - (void)map:(NSString *)router toClass:(Class __nonnull)vcClass;
+
+/**
+ 证据给定的短连接，查找对应的对象类型
+
+ @param router 短连接
+ @return 被注册好的类型，如果未被注册，则返回空类型
+ */
 - (id _Nullable)classMatchRouter:(NSString *)router;
 
 
--- FFHeros/Components/Router/ffRouterHelper.h
+++ FFHeros/Components/Router/ffRouterHelper.h
@@ -8,8 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+DEPRECATED_MSG_ATTRIBUTE("标准RESTful链接翻译成短连接的帮助类，未完成，禁止使用")
 @interface ffRouterHelper : NSObject
 
-+ (NSString *)routerFromURL:(NSString *)url;
+/**
+ 将标准的RESTful链接转译成为应用内的软连接
+
+ @param url 外部调用链接
+ @return 内部使用的短连接
+ */
++ (NSString *)routerFromURL:(NSString *)url DEPRECATED_ATTRIBUTE;
 
 @end
